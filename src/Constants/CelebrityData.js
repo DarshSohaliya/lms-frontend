3993cfdaa1a6781c96699d96402aa77bdfbcc727
Darsh Sohaliya <darshsohaliya029290@gmail.com>
2024-02-22 10:39:28 +0530

    Clean up about us page

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,41 @@
+import apj from '../assets/Images/apj.png'
+import billGates from '../assets/Images/billGates.png'
+import einstein from '../assets/Images/einstein.png'
+import nelsonMandela from '../assets/Images/nelsonMandela.png'
+import steveJobs from '../assets/Images/steveJobs.png'
+
+
+
+export const celebrities = [
+    {
+        title:'Nalson Mandela',
+        description:'“Education is the most powerful weapon which you can use to change the world.”',
+        image:nelsonMandela,
+        slideNumber:1
+    },
+    {
+        title:'APJ Abdul Kalam',
+        description:'“Education is the most powerful weapon which you can use to change the world.”',
+        image:apj,
+        slideNumber:2
+    },
+    {
+        title:'Bill Gates',
+        description:'“Patience is a key element of success.”',
+        image:billGates,
+        slideNumber:3
+    },
+    {
+        title:'Steve Jobs',
+        description:'Be a yardstick of quality.',
+        image:steveJobs,
+        slideNumber:4
+    },
+    {
+        title:'Einstein',
+        description:'Learn from yesterday, live for today, hope for tomorrow...',
+        image:einstein,
+        slideNumber:5
+    },
+   
+  ]
--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,12 +1,10 @@
 import HomeLayout from "../Layouts/HomeLayout"
 import aboutMainImage from '../assets/Images/aboutMainImage.png'
-import apj from '../assets/Images/apj.png'
-import billGates from '../assets/Images/billGates.png'
-import einstein from '../assets/Images/einstein.png'
-import nelsonMandela from '../assets/Images/nelsonMandela.png'
-import steveJobs from '../assets/Images/steveJobs.png'
+import { celebrities } from "../Constants/CelebrityData"
+import CarouselSlide from "../components/CarouselSlide"
 
 function AboutUs() {
+
     return (
           <HomeLayout>
             <div className="  pl-20 pt-20 flex flex-col text-white">
@@ -32,74 +30,10 @@ function AboutUs() {
 
                 </div>
   <div className="carousel w-1/2 my-16 m-auto">
-  <div id="slide1" className="carousel-item relative w-full">
-   <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-   <img src={apj} className="w-40 rounded-full border-2 border-gray-400" />
-   <p className="text-xl text-gray-200">
-    {"“To succeed in your mission, you must have single-minded devotion to your goal.”"}
-   </p>
-   <h3 className="text-2xl font-semibold">APJ Abdul Kalam</h3>
-    <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide5" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-   </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-    <img src={billGates} className="w-40 rounded-full border-2 border-gray-400" />
-    <p className="text-xl text-gray-200">
-    {"“Patience is a key element of success.”"}
-   </p>
-   <h3 className="text-2xl font-semibold">Bill Gates</h3>
-    <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-   <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-   <img src={steveJobs} className="w-40 rounded-full border-2 border-gray-400" />
-   <p className="text-xl text-gray-200">
-    {"Be a yardstick of quality."}
-   </p>
-   <h3 className="text-2xl font-semibold">Steve Jobs</h3>
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle ">❯</a>
-    </div>
-   </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-  <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-    <img src={einstein} className="w-40 rounded-full border-2 border-gray-400" />
-    <p className="text-xl text-gray-200">
-    {"Learn from yesterday, live for today, hope for tomorrow..."}
-   </p>
-   <h3 className="text-2xl font-semibold">Einstein</h3>
-    <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide5" className="btn btn-circle">❯</a>
-    </div>
-    </div>
-    </div>
-   
-    <div id="slide5" className="carousel-item relative w-full">
-   <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
-   <img src={nelsonMandela} className="w-40 rounded-full border-2 border-gray-400" />
-   <p className="text-xl text-gray-200">
-    {"“Education is the most powerful weapon which you can use to change the world.” "}
-   </p>
-   <h3 className="text-2xl font-semibold">Nelson Mandela</h3>
-    <div className="absolute  flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-   </div>
+    {celebrities && celebrities.map(celebrity => (<CarouselSlide {...celebrity} key={celebrity.slideNumber} totalSlides={celebrities.length}/>))}
+     
   </div>
-</div>
-</div>
+   </div>
 
             </HomeLayout>
     )
