5e2d772dad42343b3961bb33e46cbb6188b4004c
Darsh Sohaliya <darshsohaliya029290@gmail.com>
2024-02-25 12:44:48 +0530

    Add Email Functionality and solve error in my backend

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import HomeLayout from "../Layouts/HomeLayout"
 import toast from "react-hot-toast";
 import { isEmail } from "../Helpers/regexMatcher";
+import axiosInstance from "../Helpers/axiosInstance";
 
 function Contact() {
 
@@ -28,7 +29,31 @@ function Contact() {
                      return
               }
 
-            
+         if (!isEmail(userInput.email)) {
+            toast.error("Invalid Email")
+            return
+         }   
+        
+         try {
+            console.log("ghj");
+            const response = axiosInstance.post('/contact',userInput)
+           toast.promise(response,{
+            loading:"Submitting your message...",
+            success:"Form submitted successfully",
+            error:"Failed to submit the form"
+           })
+           console.log(response);
+           const contactResponse = await response
+           if (contactResponse?.success) {
+             setUserInput({
+                name:"",
+                email:"",
+                message:""
+             })
+           }
+         } catch (error) {
+            toast.error("Operation Failed")
+         }
    }
 
      return(
@@ -40,15 +65,15 @@ function Contact() {
                </h1>
                <div className="flex flex-col w-full gap-1">
                   <label htmlFor="name" className="text-xl font-semibold">Name</label>
-                  <input type="text" name="name" id="name" onChange={handleInputChange} placeholder="Enter Your Name" className="bg-transparent border px-2 py-1 rounded-sm"/>
+                  <input type="text" name="name" id="name" onChange={handleInputChange} value={userInput.name} placeholder="Enter Your Name" className="bg-transparent border px-2 py-1 rounded-sm"/>
                </div>
                <div className="flex flex-col w-full gap-1">
                   <label htmlFor="email" className="text-xl font-semibold">Email</label>
-                  <input type="text" name="email" id="email" onChange={handleInputChange} placeholder="Enter Your Email" className="bg-transparent border px-2 py-1 rounded-sm"/>
+                  <input type="text" name="email" id="email" onChange={handleInputChange} value={userInput.email} placeholder="Enter Your Email" className="bg-transparent border px-2 py-1 rounded-sm"/>
                </div>     
                <div className="flex flex-col w-full gap-1">
                   <label htmlFor="message" className="text-xl font-semibold">Message</label>
-                  <textarea  name="message" id="message" onChange={handleInputChange} placeholder="Enter Your Message" className="bg-transparent border px-2 py-1 rounded-sm resize-none h-40"/>
+                  <textarea  name="message" id="message" onChange={handleInputChange} value={userInput.message} placeholder="Enter Your Message" className="bg-transparent border px-2 py-1 rounded-sm resize-none h-40"/>
                </div>     
             
                 <button className="w-full bg-yellow-600 hover:bg-yellow-500 transition-all duration-300 rounded-sm py-2 font-semibold text-lg cursor-pointer" type="submit">
